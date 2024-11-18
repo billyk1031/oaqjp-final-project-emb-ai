8260f6ef68141e2b4e90ddf6fb8cab1bfbfea4e4
billyk1031 <billykan@gmail.com>
2024-11-18 16:31:49 -0500

    Task 8 Completed

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,7 +5,16 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     jsonobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = jsonobj, headers=headers)
-
+    
+    # check error code
+    if response.status_code == 400:
+        return {'anger': None,
+                'disgust': None,
+                'fear': None,
+                'joy': None,
+                'sadness': None,
+                'dominant_emotion': None}
+                
     # retrieving emotion scores from response
     formatted_response = json.loads(response.text)
     anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
